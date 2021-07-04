1b669ced68791674a1bdb3bca52d8f744a2b2676
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-07-04 22:54:12 +0200

    Improve indent.

--- src/app.py
+++ src/app.py
@@ -5,23 +5,22 @@ from faker import Faker
 
 
 def handler(event, context):
-  try:
-      # Import library to insert fake datas.
-      fake = Faker()
+    try:
+        # Import library to insert fake datas.
+        fake = Faker()
 
-      # Generate fake books data.
-      payload = {
-          "book": {
-              "isbn": str(fake.isbn13()),
-              "title": str(fake.sentence(nb_words=5)),
-              "author": str(fake.name()),
-              "stock": str(random.randint(0, 20)),
-          }
-      }
+        # Generate fake books data.
+        payload = {
+            "book": {
+                "isbn": str(fake.isbn13()),
+                "title": str(fake.sentence(nb_words=5)),
+                "author": str(fake.name()),
+                "stock": str(random.randint(0, 20)),
+            }
+        }
 
-      return {"statusCode": 200, "body": {"body": json.dumps(payload)}}
+        return {"statusCode": 200, "body": {"body": json.dumps(payload)}}
 
-
-  except:
-    print("[ERROR] " + str(sys.exc_info()[1]))
-    return {"statusCode": 500, "body": {"message": str(sys.exc_info()[1])}}
+    except:
+        print("[ERROR] " + str(sys.exc_info()[1]))
+        return {"statusCode": 500, "body": {"message": str(sys.exc_info()[1])}}
